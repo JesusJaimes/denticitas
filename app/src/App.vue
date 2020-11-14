aa83d823ca27c8f70ab9e77922d64d3cc908e2e1
Romario <romariojaimes@gmail.com>
2020-11-14 17:41:04 -0500

    Update .browserslistrc, .eslintrc.js, and 24 more files...

---
+++ app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class='leftSideBarLayout'>
+      <router-view name='sideMenu'></router-view>
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  @import './assets/style.css';
+</style>
